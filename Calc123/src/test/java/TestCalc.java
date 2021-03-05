aceb01e981aad1ca3e051a59a57703b15e77df29
paulcourt <77756501+paulcourt@users.noreply.github.com>
2021-03-05 11:07:36 -0600

    Update TestCalc.java

--- Calc123/src/test/java/TestCalc.java
+++ Calc123/src/test/java/TestCalc.java
@@ -8,28 +8,28 @@ public class TestCalc {
 @Test
 public void testAdd() {
 	Calc test = new Calc();
-	int result = test.add(3, 2);
-	assertEquals(5, result);
+	double result = test.add(3, 2);
+	assertEquals(5, result, 0.01);
 }
 
 @Test
 public void testSubt() {
 	Calc test = new Calc();
-	int result = test.subt(3, 2);
-	assertEquals(1, result);
+	double result = test.subt(3, 2);
+	assertEquals(1, result, 0.01);
 }
 
 @Test
 public void testMult() {
 	Calc test = new Calc();
-	int result = test.mult(3, 2);
-	assertEquals(6, result);
+	double result = test.mult(3, 2);
+	assertEquals(6, result, 0.01);
 }
 
 @Test
 public void testDiv() {
 	Calc test = new Calc();
-	int result = test.div(3, 2);
-	assertEquals(1.5, result);
+	double result = test.div(3, 2);
+	assertEquals(1.5, result, 0.01);
 }
 }
