b1cefd46f707a3e83bc67da02e7c7cfcec1d66da
paulcourt <77756501+paulcourt@users.noreply.github.com>
2021-03-05 16:49:27 -0600

    Update Calc.java

--- Calc123/src/main/java/Calc.java
+++ Calc123/src/main/java/Calc.java
@@ -1,47 +1,50 @@
-public class Calc{
-	
-	public static double add(double a, double b) {
-		return a + b;
+package Calc;
+
+	public class Calc{
 		
-	}
-	public static double subt(double a, double b) {
-		return a - b;
+		public static double add(double a, double b) {
+			return a + b;
+			
+		}
+		public static double subt(double a, double b) {
+			return a - b;
+			
+		}		
 		
-	}		
-	
-	public static double mult(double a, double b) {
-		return a * b;
+		public static double mult(double a, double b) {
+			return a * b;
+			
+		}		
 		
-	}		
-	
-	public static double div(double a, double b) {
-		if (b == 0) {
+		public static double div(double a, double b) {
+			if (b == 0) {
 	            throw new ArithmeticException("Division by 0");
 			}
 	            else{
 	            return a / b;
 	        }
-	}
-	
-	
-	public static void main (String[] argv) 
-	   {
+			
+		}
 		
-		double result = add(3,2);
-	     
-	      System.out.println("Add: " + result);
-
-		result = Calc.subt(3, 2);
+		
+		public static void main (String[] argv) 
+		   {
+			
+			double result = add(2,3);
 		     
-		  System.out.println("Subt: " + result);
-		
-		result = Calc.mult(3,2);
+		      System.out.println("Add: " + result);
+
+			result = Calc.subt(2,3);
 			     
-			System.out.println("Mult: " + result);
-
-		result = Calc.div(3,2);
+			  System.out.println("Subt: " + result);
+			
+			result = Calc.mult(2,3);
 				     
-			System.out.println("Div: " + result);
+				System.out.println("Mult: " + result);
 
-	   }
-}
+			result = Calc.div(2,0);
+					     
+				System.out.println("Div: " + result);
+
+		   }
+	}
