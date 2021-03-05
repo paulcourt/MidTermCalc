c58975d7b796bf7cc1c4ac92d1752a0fce80e1a1
paulcourt <77756501+paulcourt@users.noreply.github.com>
2021-03-05 15:13:28 -0600

    Update TestCalc.java
    Added the divide by zero test.

--- Calc123/src/test/java/TestCalc.java
+++ Calc123/src/test/java/TestCalc.java
@@ -1,7 +1,8 @@
-
+/** These test cases satisfy question 3 of Mid Term Exam for SE 670
+ * 
+ */
 import static org.junit.Assert.*;
 import org.junit.Test;
-
 
 public class TestCalc {
 	
@@ -9,34 +10,44 @@ public class TestCalc {
 public void testAdd() {
 	Calc test = new Calc();
 	double result = test.add(3, 2);
-	assertEquals(5, result, 0.01);
+	assertEquals(5, result, 0.1);
 }
 
 @Test
 public void testSubt() {
 	Calc test = new Calc();
 	double result = test.subt(3, 2);
-	assertEquals(1, result, 0.01);
+	assertEquals(-1, result, 0.1);
 }
 
 @Test
 public void testMult() {
 	Calc test = new Calc();
 	double result = test.mult(3, 2);
-	assertEquals(6, result, 0.01);
+	assertEquals(6, result, 0.1);
 }
 
+/** The next three tests are for the divide function.
+ * 
+ */
 @Test
-public void testDiv() {
+public void testDiv1() {
 	Calc test = new Calc();
 	double result = test.div(3, 2);
-	assertEquals(1.5, result, 0.01);
+	assertEquals(1.5, result, 0.1);
 }
+
 @Test
 public void testDiv2() {
 	Calc test = new Calc();
 	double result = test.div(2, 3);
-	assertEquals(0.66, result, 0.01);
+	assertEquals(0.66, result, 0.1);
 }
-	
+
+@Test(expected = IllegalArgumentException.class)
+public void testDiv3() {
+	Calc test = new Calc();
+	double result = test.div(3, 0);
+}
+
 }
