2642a1abfa104ac718576a13c3b8dc5fd093fd7e
paulcourt <77756501+paulcourt@users.noreply.github.com>
2021-03-05 10:40:32 -0600

    Update TestCalc.java
    Test cases in original run should show that divide function is incorrectly done.

--- Calc123/src/test/java/TestCalc.java
+++ Calc123/src/test/java/TestCalc.java
@@ -8,28 +8,28 @@ public class TestCalc {
 @Test
 public void testAdd() {
 	Calc test = new Calc();
-	int result = test.add(2, 3);
+	int result = test.add(3, 2);
 	assertEquals(5, result);
 }
 
 @Test
 public void testSubt() {
 	Calc test = new Calc();
-	int result = test.subt(2, 3);
-	assertEquals(-1, result);
+	int result = test.subt(3, 2);
+	assertEquals(1, result);
 }
 
 @Test
 public void testMult() {
 	Calc test = new Calc();
-	int result = test.mult(2, 3);
+	int result = test.mult(3, 2);
 	assertEquals(6, result);
 }
 
 @Test
 public void testDiv() {
 	Calc test = new Calc();
-	double result = test.div(2, 3);
-	assertEquals(0.666, result);
+	result = test.div(3, 2);
+	assertEquals(1.5, result);
 }
 }
