57242e3924429fdafdcdb1b43e39f3b8f77404ca
paulcourt <77756501+paulcourt@users.noreply.github.com>
2021-03-05 17:37:13 -0600

    Update TestCalc.java

--- Calc123/src/test/java/TestCalc.java
+++ Calc123/src/test/java/TestCalc.java
@@ -1,63 +1,46 @@
-/** These test cases satisfy question 3 of Mid Term Exam for SE 670
- * 
- */
-import static org.junit.Assert.*;
 
-import org.junit.Before;
+import static org.junit.Assert.*;
 import org.junit.Test;
-import org.junit.Before;
 
 
 public class TestCalc {
 	
-	private Calc test;
-	
-	@Before
-	public void setUp() throws Exception {
-		test = new Calc();
-	}
-	
 @Test
 public void testAdd() {
-
+	Calc test = new Calc();
 	double result = test.add(3, 2);
-	assertEquals(5, result, 0.1);
+	assertEquals(5, result, 0.01);
 }
 
 @Test
 public void testSubt() {
-
+	Calc test = new Calc();
 	double result = test.subt(3, 2);
-	assertEquals(-1, result, 0.1);
+	assertEquals(1, result, 0.01);
 }
 
 @Test
 public void testMult() {
-
+	Calc test = new Calc();
 	double result = test.mult(3, 2);
-	assertEquals(6, result, 0.1);
+	assertEquals(6, result, 0.01);
 }
 
-/** The next three tests are for the divide function.
- * 
- */
 @Test
-public void testDiv1() {
-
+public void testDiv() {
+	Calc test = new Calc();
 	double result = test.div(3, 2);
-	assertEquals(1.5, result, 0.1);
+	assertEquals(1.5, result, 0.01);
 }
-
 @Test
 public void testDiv2() {
-
+	Calc test = new Calc();
 	double result = test.div(2, 3);
-	assertEquals(0.66, result, 0.1);
+	assertEquals(0.66, result, 0.01);
 }
-
 @Test(expected = java.lang.ArithmeticException.class)
 public void testDiv3() {
-	double result = test.div(3, 0);
-}
-
+	Calc test = new Calc();
+	double result = test.div(3, 0);		
+}	
 }
