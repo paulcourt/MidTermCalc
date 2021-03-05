6d9d9c33b574280c561760d269c07b27964782a4
paulcourt <77756501+paulcourt@users.noreply.github.com>
2021-03-05 16:52:28 -0600

    Update TestCalc.java

--- Calc123/src/test/java/TestCalc.java
+++ Calc123/src/test/java/TestCalc.java
@@ -1,55 +1,56 @@
-/** These test cases satisfy question 3 of Mid Term Exam for SE 670
- * 
- */
-import static org.junit.Assert.*;
+package Calc;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+import org.junit.Assert.*;
 import org.junit.Test;
 import org.junit.Before;
 
-public class TestCalc {
-
-   @Before
-public void setUp() throws Exception {}
+public class JUnitTests {
 	
-@Test
-public void testAdd() {
-	Calc test = new Calc();
-	double result = test.add(3, 2);
-	assertEquals(5, result, 0.1);
-}
+	private Calc test;
+	
+	@Before
+	public void setUp() throws Exception {
+		test = new Calc();
+	}
+		
+	@Test
+	public void testAdd() {
+		double result = test.add(3, 2);
+		assertEquals(5, result, 0.1);
+	}
 
-@Test
-public void testSubt() {
-	Calc test = new Calc();
-	double result = test.subt(3, 2);
-	assertEquals(1, result, 0.1);
-}
+	@Test
+	public void testSubt() {
+		double result = test.subt(3, 2);
+		assertEquals(1, result, 0.1);
+	}
 
-@Test
-public void testMult() {
-	Calc test = new Calc();
-	double result = test.mult(3, 2);
-	assertEquals(6, result, 0.1);
-}
+	@Test
+	public void testMult() {
+		double result = test.mult(3, 2);
+		assertEquals(6, result, 0.1);
+	}
 
-/** The next three tests are for the divide function.
- * 
- */
-@Test
-public void testDiv1() {
-	Calc test = new Calc();
-	double result = test.div(3, 2);
-	assertEquals(1.5, result, 0.1);
-}
+	/** The next three tests are for the divide function.
+	 * 
+	 */
+	@Test
+	public void testDiv1() {
+		double result = test.div(3, 2);
+		assertEquals(1.5, result, 0.1);
+	}
 
-@Test
-public void testDiv2() {
-	Calc test = new Calc();
-	double result = test.div(2, 3);
-	assertEquals(0.66, result, 0.1);
-}
+	@Test
+	public void testDiv2() {
+		double result = test.div(2, 3);
+		assertEquals(0.66, result, 0.1);
+	}
 
-@Test(expected = java.lang.ArithmeticException.class)
+	@Test(expected = java.lang.ArithmeticException.class)
 	public void testDiv3() {
-	double result = test.div(3, 0);
+			 double result = test.div(3, 0);
 }
 }
